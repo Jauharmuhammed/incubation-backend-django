7afee80a59c984efa3ee2b2e42d37e3fcc7fc21b
Jauhar Muhammed <94921244+Jauharmuhammed@users.noreply.github.com>
2023-10-06 19:25:19 +0530

    railway.json added

--- config/settings.py
+++ config/settings.py
@@ -202,3 +202,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     "http://localhost:3000",
 #     "http://127.0.0.1:3000",
 # ]
+
+CSRF_TRUSTED_ORIGINS = ['https://incubation-backend-django-production.up.railway.app']
